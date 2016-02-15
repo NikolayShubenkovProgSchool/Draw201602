ed6658b82b39c19fa84a34a60935d673ff7d2407
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2016-02-15 21:11:16 +0300

    rotate context

--- CreateImageViewController.swift
+++ CreateImageViewController.swift
@@ -29,8 +29,24 @@ class CreateImageViewController: UIViewController {
         let context = UIGraphicsGetCurrentContext()!
         
         drawGradientOfSize(imageView.frame.size, context: context)
+
+        for _ in 0...5 {
+            
+            let angle:CGFloat = CGFloat(M_PI * 2.0 / 6.0)
+            
+            drawRadialGradientOfSize(imageView.frame.size, context: context)
+            
+            CGContextRotateCTM(context, angle)
+//            CGContextScaleCTM(context, 0.9, 0.8)
+            
+        }
+
         
         drawRadialGradientOfSize(imageView.frame.size, context: context)
+        
+        
+        crossTheHoleImageOfSize(imageView.frame.size)
+        
         
         let image = UIGraphicsGetImageFromCurrentImageContext()
         
@@ -66,6 +82,8 @@ class CreateImageViewController: UIViewController {
     
     func drawRadialGradientOfSize(size:CGSize,context:CGContextRef){
         
+        CGContextSaveGState(context)
+        
         let clipPath = UIBezierPath(roundedRect: CGRectMake(40, 40, size.width/2, size.width/1.5), byRoundingCorners: [.BottomLeft,.TopLeft], cornerRadii:CGSizeMake(10, 10))
         
         clipPath.addClip()
@@ -81,15 +99,38 @@ class CreateImageViewController: UIViewController {
         let startPoint = CGPointMake(40, 40)
         let finishPoint = CGPointMake(60, 60)
         
-        
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         
         let gradient = CGGradientCreateWithColors(colorSpace, colors, positions)
         
         CGContextDrawRadialGradient(context, gradient, startPoint, 80, finishPoint, 10, [])
         
+        CGContextRestoreGState(context)
+    }
+
+    func crossTheHoleImageOfSize(size:CGSize){
+        
+        let crossPath = UIBezierPath()
+        
+        crossPath.moveToPoint(CGPointMake(0, 0))
+        crossPath.addLineToPoint(CGPointMake(size.width, size.height))
+        crossPath.moveToPoint(CGPointMake(size.width, 0))
+        crossPath.addLineToPoint(CGPointMake(0, size.height))
+        
+        crossPath.lineWidth = 3
+        
+        UIColor.blackColor().setStroke()
+        
+        crossPath.stroke()
         
     }
+    
+    
+    
+    
+    
+    
+    
     
     
     
